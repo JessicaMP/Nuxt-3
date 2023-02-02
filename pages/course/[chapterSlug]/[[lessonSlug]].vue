4daf7af5d8f0dfc9e5316a39f8076edd7fa39b0d
Jessica <jessica95.mp@gmail.com>
2023-02-01 21:20:05 -0500

    feat: condicionales para mostrar los slugs si estan en la ruta

---
+++ pages/course/[chapterSlug]/[[lessonSlug]].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Lesson</h2>
+    <p>This is a lesson</p>
+    <p v-if="$route.params.chapterSlug">Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p v-if="$route.params.lessonSlug">Lesson slug: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Hello, Mastering Nuxt 3
+    </div>
+</template>
