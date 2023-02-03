579964d9af2299c9eea67680d83a86344626bfb9
Jessica <jessica95.mp@gmail.com>
2023-02-02 22:28:52 -0500

    feat: agregando mock de data de capitulos y clase 1-5 Loading in Course Data completa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      'composables',
+      // ... or scan modules nested one level deep with a specific name and file extension
+      'composables/*/index.{ts,js,mjs,mts}',
+      // ... or scan all modules within given directory
+      'composables/**'
+    ]
+  }
 });
