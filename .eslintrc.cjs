6ef8292c98000ee80d8d54b16bec4f27dd37b696
Jessica <jessica95.mp@gmail.com>
2023-02-05 20:06:07 -0500

    feat: reemplazo de la instalacion pnpm por npm

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: ["@nuxt/eslint-config", "@nuxtjs/eslint-config-typescript", "some-other-config-you-use",
+    "prettier"],
+    "rules": {
+        "indent": "error"
+      }
+  };
--- pages/course/[chapterSlug]/[[lessonSlug]].vue
+++ pages/course/[chapterSlug]/[[lessonSlug]].vue
@@ -3,5 +3,8 @@
 </script>
 
 <template>
+  <!-- <div> -->
   <Lesson />
+  <!-- aaaaa
+</div> -->
 </template>
